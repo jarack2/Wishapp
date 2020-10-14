0306d6fddad964f975451dd69f7c10ce52cbc6f6
98khongmin <98khongmin@gmail.com>
2020-10-14 11:45:45 -0600

    App.js in progress #20

--- App.js
+++ App.js
@@ -13,15 +13,28 @@ import { createStackNavigator } from '@react-navigation/stack';
 const AuthStack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
+function TabNav() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen}/>
+      <Tab.Screen name="Sign Up" component={SignUpScreen} />
+      <Tab.Screen name="Login" component={Login} />
+      <Tab.Screen name="LandingPage" component={LandingPage} />
+    </Tab.Navigator>
+  );
+}
+
 export default function App() {
   return (
     <NavigationContainer>
-      <AuthStack.Navigator>
+      {/* <AuthStack.Navigator>
+        <AuthStack.Screen name="TabNav" component={TabNav} />
         <AuthStack.Screen name="Home" component={HomeScreen} />
         <AuthStack.Screen name="Sign Up" component={SignUpScreen} />
         <AuthStack.Screen name="Login" component={Login} />
         <AuthStack.Screen name="LandingPage" component={LandingPage} />
-      </AuthStack.Navigator>
+      </AuthStack.Navigator> */}
+      <TabNav />
     </NavigationContainer> 
   );
   
