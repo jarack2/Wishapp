4ad0f70c778274c4d0ba0232ffed3ea1be929988
Steven Kim <47097113+98khongmin@users.noreply.github.com>
2020-11-01 21:37:15 -0700

    Update Landing Page

--- src/screens/LandingPage.js
+++ src/screens/LandingPage.js
@@ -12,11 +12,6 @@ import {
   ScrollView,
   FlatList,
   TouchableOpacity,
-<<<<<<< Updated upstream
-=======
-  Modal,
-  TouchableHighlight,
->>>>>>> Stashed changes
 } from 'react-native';
 
 import {
@@ -92,7 +87,7 @@ const LandingPage = (props) => {
         
         <TouchableOpacity style={{ flexDirection: "row", alignItems: 'center', justifyContent: 'center', padding: 20 }}
           activeOpacity={0.5}>
-          <Image source={require('../assets/avatar_default.png')} style={styles.topBar} />
+          <Image source={require('../assets/plus.png')} style={styles.topBar} />
           </TouchableOpacity>
 
           <TouchableOpacity style={{ flexDirection: "row", alignItems: 'center', justifyContent: 'center', padding: 20 }}
