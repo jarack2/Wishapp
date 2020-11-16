8d43fdd397ad57179b27585975903a59e1a2d053
root <georgerschmid@gmail.com>
2020-11-16 00:30:34 +0000

    closes #60

--- src/screens/settings.js
+++ src/screens/settings.js
@@ -4,6 +4,7 @@ import {
   Text,
   View,
   StatusBar,
+  ImageBackground,
   Image,
   Platform,
   RefreshControl,
@@ -14,55 +15,68 @@ import {
 
 const screenWidth = Dimensions.get("window").width;
 const screenHeight = Dimensions.get("window").height;
-
+const image = { url: '../assets/background.png' };
 export default class settings extends Component {
+  
   _onPressButton() {
     alert('You tapped the button!')
   }
   render(){
     return (
       <>
-       <Text style={styles.title}>Settings</Text>
-
-       <View style={{flex: 1,padding :20 ,flexDirection: 'column',justifyContent: 'left',alignItems: 'left',}}>
-       <Image
-          source={require('../assets/avatar_default.png')}
-          style={{ width: screenHeight/8, height: screenHeight/8, borderRadius: 200 / 2, marginHorizontal: 10}}
-        ></Image>
-        </View>
-      
+       <ImageBackground
+        source={require('../assets/background.png')}
+        style={styles.image}
+      >
       <View style={{
         flex: 1,
         flexDirection: 'column',
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
         alignItems: 'stretch',
+        
       }}>
-        <View>
-          <Button
-            onPress={this._onPressButton}
-            title="Enable Dark Mode"
-          />
-          <Button
-            onPress={this._onPressButton}
-            title="Press Me"
-            color="#841584"
-          />
-          <Button
-            onPress={this._onPressButton}
-            title="This looks great!"
-          />
-          <Button
-            onPress={this._onPressButton}
-            title="OK!"
-            color="#841584"
-          />
-          <Button
-            onPress={this._onPressButton}
-            title="OK!"
-            color="#841584"
-          />
-        </View>
+         <Text style={styles.title}>Settings</Text>
+
+<View style={{flex: 1,padding :20 ,flexDirection: 'row',justifyContent: 'left',alignItems: 'left',}}>
+<Image
+   source={require('../assets/avatar_default.png')}
+   style={{ width: screenHeight/8, height: screenHeight/8, borderRadius: 200 / 2, marginHorizontal: 10}}
+ ></Image>
+   <View style={{flex: 1,padding :20 ,flexDirection: 'column',justifyContent: 'left',alignItems: 'left',}}>
+    <Text> Email: </Text>
+    <Text> Username: </Text>
+  </View>
+ </View>
+ 
+   <Button
+     onPress={this._onPressButton}
+     title="Enable Dark Mode"
+   />
+   <Button
+     onPress={this._onPressButton}
+     title="Press Me"
+     color="#841584"
+   />
+   <Button
+     onPress={this._onPressButton}
+     title="This looks great!"
+   />
+   <Button
+     onPress={this._onPressButton}
+     title="OK!"
+     color="#841584"
+   />
+   <Button
+     onPress={this._onPressButton}
+     title="OK!"
+     color="#841584"
+   />
+ <View style={{width: 50, height: 350}} />
+      
+      <Text style={styles.title}>bottom</Text>
+
       </View>
+      </ImageBackground>
       </>
     );
   }
@@ -73,16 +87,15 @@ export default class settings extends Component {
       title: {
         fontSize: '48px',
         color: 'white',
-        marginVertical: '80px',
         textAlign: 'center',
       },
       container: {
-        paddingTop: 60,
+        //paddingTop: 60,
         alignItems: 'center'
       },
       button: {
-        marginBottom: 30,
-        height : 100,
+        //marginBottom: 1030,
+       // height : 100,
         alignItems: 'center',
         backgroundColor: '#2196F3'
       },
@@ -92,7 +105,7 @@ export default class settings extends Component {
       },
       buttonText: {
         textAlign: 'center',
-        padding: 20,
+        //padding: 20,
         color: 'white'
       }
   });
