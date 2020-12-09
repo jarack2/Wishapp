2ccc98822d481701773e7c8847e39f206651a9ab
Jared <jared.a.rackley@gmail.com>
2020-12-09 10:29:29 -0800

    modal skeleton

---
+++ src/components/Modal.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Modal as XModal, StyleSheet, TouchableHighlight } from 'react-native';
+
+const Modal = () => {
+  return <XModal></XModal>;
+};
+
+export default Modal;
