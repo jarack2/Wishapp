ec9dff48d106ee46f3de0d28bd9064193270066b
Jared <jared.a.rackley@gmail.com>
2020-12-12 03:01:15 -0800

    implements #74

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,29 +1,99 @@
-import React, { Component } from 'react';
-import { Button, Text, View } from 'react-native';
-import Modal from 'react-native-modal';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import {
+  Alert,
+  Modal as XModal,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View,
+} from 'react-native';
 
-export default class ModalTester extends Component {
-  state = {
-    isModalVisible: false,
-  };
+import { BackButton, Header } from '../components';
 
-  toggleModal = () => {
-    this.setState({ isModalVisible: !this.state.isModalVisible });
-  };
+const Modal = ({ children, title, visible, setVisible }) => {
+  return (
+    <View style={styles.centeredView}>
+      <XModal
+        animationType="slide"
+        transparent={true}
+        visible={visible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+        }}
+      >
+        <Header titleStyles={{ textAlign: 'center' }} title={title}>
+          <BackButton
+            onPress={() => {
+              setVisible(!visible);
+            }}
+          />
+          {children}
+          <View style={styles.centeredView}>
+            <TouchableHighlight
+              style={styles.closeButton}
+              onPress={() => {
+                setVisible(!visible);
+              }}
+            >
+              <Text style={styles.textStyle}>Close Modal</Text>
+            </TouchableHighlight>
+          </View>
+        </Header>
+      </XModal>
+    </View>
+  );
+};
 
-  render() {
-    return (
-      <View style={{ flex: 1 }}>
-        <Button title="Show modal" onPress={this.toggleModal} />
-        {this.state.isModalVisible ? (
-          <Modal isVisible={this.state.isModalVisible}>
-            <View style={{ flex: 1 }}>
-              <Text>Hello!</Text>
-              <Button title="Hide modal" onPress={this.toggleModal} />
-            </View>
-          </Modal>
-        ) : undefined}
-      </View>
-    );
-  }
-}
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    flex: 1,
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  closeButton: {
+    backgroundColor: '#F194FF',
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+    maxWidth: 128,
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+});
+
+Modal.propTypes = {
+  /* The content of the modal. */
+  children: PropTypes.node,
+  /* The title for the modal. */
+  title: PropTypes.string,
+  /* Props to tell whether the modal is visible or not, controlled from an outside source. */
+  setVisible: PropTypes.func,
+  visible: PropTypes.bool,
+};
+
+export default Modal;
